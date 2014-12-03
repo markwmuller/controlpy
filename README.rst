1a10bf67483ac927d1b5c9061167e4154a19c849
Mark Müller <mwm@ethz.ch>
2014-12-03 09:59:20 +0100

    Just added some more outline files.

---
+++ README.rst
@@ -0,0 +1,6 @@
+Controlpy
+---------
+
+todo - description
+
+
--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup
 
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
 setup(name='controlpy',
       version='0.0',
       description='Python controls',
