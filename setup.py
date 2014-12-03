16252c49c8a815b74a6d7c3401d547331de16e05
Mark Müller <mwm@ethz.ch>
2014-12-03 09:49:22 +0100

    Created barebones outline

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='controlpy',
+      version='0.0',
+      description='Python controls',
+      url='http://github.com/markwmuller/controlpy',
+      author='Mark Mueller',
+      author_email='mwm@mwm.im',
+      license='gplv3',
+      packages=['controlpy'],
+      zip_safe=False)
