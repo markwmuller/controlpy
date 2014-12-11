b81ed346d7f85b58bd51665eb8a011b78758a2c1
Mark Muller <mark@localhost.localdomain>
2014-12-11 23:08:03 +0100

    Created some examples, and extended the readme a little.

---
+++ examples/example_controllability.py
@@ -0,0 +1,27 @@
+'''Example showing how to test a system's controllability
+'''
+
+from __future__ import print_function, division
+
+import controlpy
+
+import numpy as np
+
+# A single input system, one uncontrollable mode.
+A = np.matrix([[0,1,0],[0,0,1],[0,0,5]])
+B = np.matrix([[0],[1],[0]])
+
+uncontrollableModes = controlpy.controllability.uncontrollable_modes(A,B)
+
+if not uncontrollableModes:
+    print('System is controllable.')
+else: 
+    print('System is uncontrollable. Uncontrollable modes are:')
+    print(uncontrollableModes)
+
+
+
+
+
+
+
---
+++ examples/example_lqr.py
@@ -0,0 +1,30 @@
+'''A simple example script, which implements an LQR controller for a double integrator.
+'''
+
+from __future__ import print_function, division
+
+import controlpy
+
+import numpy as np
+
+# Example system is a double integrator:
+A = np.matrix([[0,1],[0,0]])
+B = np.matrix([[0],[1]])
+
+# Define our costs:
+Q = np.matrix([[1,0],[0,0]])
+R = np.matrix([[1]])
+
+# Compute the LQR controller
+gain, X, closedLoopEigVals = controlpy.lqr.lqr_continuous_time(A,B,Q,R)
+
+print('The computed gain is:')
+print(gain)
+
+print('The closed loop eigenvalues are:')
+print(closedLoopEigVals)
+
+
+
+
+
